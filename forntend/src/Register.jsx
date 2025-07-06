e6d1cebf024dbc4ba358f54a10e6f5b8db8770d6
Akash00421 <akashabinesh03@gmail.com>
2025-07-06 13:53:19 +0530

    change link in  frontend

--- forntend/src/Register.jsx
+++ forntend/src/Register.jsx
@@ -23,7 +23,7 @@ function Register({ setUser }) {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://bookstoreweb-wwfq.onrender.com/api/auth/login", {
+      const response = await fetch("https://bookstoreweb-wwfq.onrender.com/api/auth/register", {
 
       
         method: "POST",
