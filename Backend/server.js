d7c2b84ada24895167bded538c256974cb8fbc04
Akash00421 <akashabinesh03@gmail.com>
2025-07-06 14:40:51 +0530

    changes in uri

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,10 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI) .then(() => console.log(" MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}) .then(() => console.log(" MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err.message));
 
 // Routes
